152388323dcb6aef442c09908006f19b3530bd8a
pdcmshoe <60950596+pdcmshoe@users.noreply.github.com>
2020-02-15 11:55:56 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'Njc3OTYwMDMzNDE3Mjk3OTIw.XkhKKg.gsg0yfoiS8aY9MRY78oeGe0toPA';
+
+bot.on('ready', () =>{
+    console.log('This bot is online.')
+})
+
+bot.on('message', message=>{
+    if(message.content === "Hello there"){
+        message.reply('General Kenobi...');
+    }
+})
+
+bot.login(process.env.BOT_TOKEN);
